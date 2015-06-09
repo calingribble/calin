233ba1c97ca27a91e5e14a586fb4d5726956617f
Calin Gribble <calingribble@gmail.com>
2015-06-09 18:04:25 -0400

    WIP

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,4 +14,22 @@ angular.module('calinioApp')
       'AngularJS',
       'Karma'
     ];
+
+    $scope.skills = [
+      { "name" : "Less/CSS", "rating" : 4 },
+      { "name" : "Git", "rating" : 3 },
+      { "name" : "Vim", "rating" : 3 },
+      { "name" : "Mongoose", "rating" : 3 },
+      { "name" : "MongoDB", "rating" : 3 },
+      { "name" : "Javascript", "rating" : 4 },
+      { "name" : "HTML", "rating" : 4 },
+      { "name" : "ExpressJS", "rating" : 3 },
+      { "name" : "NodeJS", "rating" : 3 },
+      { "name" : "AngularJS", "rating" : 4 },
+      { "name" : "Ruby on Rails", "rating" : 2}
+    ];
+
+    $scope.getNumber = function(num) {
+      return new Array(num);
+    }
   });
