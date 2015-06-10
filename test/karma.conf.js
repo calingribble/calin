2b5a6f0aa42a2f57880b6d0a9e9ce1d971e7737f
Calin Gribble <calingribble@gmail.com>
2015-06-09 21:29:43 -0400

    whoops, less.js added twice

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,7 +33,6 @@ module.exports = function(config) {
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/less/dist/less.js',
       'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/less/dist/less.js',
       // endbower
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
